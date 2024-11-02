1b7595700d5572bfdca4de07468858e69efcbdf5
KKW557 <kevin557557@gmail.com>
2024-11-02 19:54:15 +0800

    sc

---
+++ src/main/java/icu/suc/kevin557/servercommands/ServerCommands.java
@@ -0,0 +1,11 @@
+package icu.suc.kevin557.servercommands;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ServerCommands implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
